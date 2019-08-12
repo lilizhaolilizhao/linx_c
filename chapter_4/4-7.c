b2c6994a7722abb919061d54868735ddde7fb0d9
lilizhao <lilizhao1@oneapm.com>
2019-08-12 21:37:39 +0800

    学习中

--- chapter_4/4-7.c
+++ chapter_4/4-7.c
@@ -4,15 +4,42 @@
 
 #include <stdio.h>
 
-void function_001();
+void function_401();
+
+void function_402();
 
 int main() {
-    function_001();
+//    function_401();
+    function_402();
 
     return 0;
 }
 
-void function_001() {
+void function_402() {
+    int a[2][4] = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8}};
+    int b[4][2], i, j;
+
+    printf("array a:\n");
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 4; j++) {
+            printf("%5d", a[i][j]);
+            b[j][i] = a[i][j];
+        }
+        printf("\n");
+    }
+
+    printf("array b:\n");
+    for (i = 0; i < 4; i++) {
+        for (j = 0; j < 2; j++) {
+            printf("%5d", b[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void function_401() {
     int a[2][4] = {{1, 2, 3, 4},
                    {5, 6, 7, 8}};
     int b[4][2], i, j;
