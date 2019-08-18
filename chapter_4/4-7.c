530d90db28b6bdea9da843e0301f5d69e48c3c74
lilizhao <lilizhao1@oneapm.com>
2019-08-18 19:15:22 +0800

    字符串测试

--- chapter_4/4-7.c
+++ chapter_4/4-7.c
@@ -28,6 +28,14 @@ void function_409();
 
 void function_410();
 
+void function_4_18();
+
+void copy_string1(char a[16], char b[20]);
+
+void copy_string2(char a[16], char string[20]);
+
+void function_char_test();
+
 char *name[7] = {"Monday", "Tuesday", "Wednesday", "Thursday",
                  "Friday", "Saturday", "Sunday"};
 char *message = "wrong input";
@@ -68,9 +76,45 @@ int main() {
 //    function_407();
 //    function_408();
 //    function_409();
-    function_410();
+//    function_410();
+//    function_4_18();
+    function_char_test();
 
     return 0;
+}
+
+void function_char_test() {
+    char a[] = "Welcome to ";
+    char *p = "Linux C Program";
+
+    puts(a);
+    puts(p);
+}
+
+void function_4_18() {
+    char a[] = "Linux C Program", b[20], c[20];
+
+    copy_string1(a, b);
+    copy_string2(a, c);
+
+    printf("%s\n%s\n%s\n", a, b, c);
+}
+
+void copy_string2(char *psrc, char *pdst) {
+    for (; *psrc != '\0'; psrc++, pdst++) {
+        *pdst = *psrc;
+    }
+
+    *pdst = '\0';
+}
+
+void copy_string1(char src[], char dst[]) {
+    int i;
+
+    for (i = 0; src[i] != '\0'; i++) {
+        dst[i] = src[i];
+    }
+    dst[i] = '\0';
 }
 
 void function_410() {
