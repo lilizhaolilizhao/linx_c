8de2b35895335bb47caac00f4663d5c027c3c9de
lilizhao <lilizhao1@oneapm.com>
2019-08-20 12:29:02 +0800

    第四章学习完毕

--- chapter_4/4-7.c
+++ chapter_4/4-7.c
@@ -3,7 +3,8 @@
 //
 
 #include <stdio.h>
-#include "string.h"
+#include <string.h>
+#include <stdlib.h>
 
 void function_401();
 
@@ -63,6 +64,12 @@ void function_4_37();
 
 char *RetMemory();
 
+void function_4_38();
+
+int findnumstring(char *p, char string[27]);
+
+void function_4_39();
+
 char *name[7] = {"Monday", "Tuesday", "Wednesday", "Thursday",
                  "Friday", "Saturday", "Sunday"};
 char *message = "wrong input";
@@ -110,13 +117,89 @@ int main() {
 //    function_4_34();
 //    function_4_35();
 //    function_4_36();
-    function_4_37();
+//    function_4_37();
+//    function_4_38();
+    function_4_39();
 
     return 0;
 }
 
+void function_4_39() {
+    char str[] = "ABCD1234efgh";
+    int length = strlen(str);
+    char *p1 = str;            // p1指向字符串的第一个字符
+    char *p2 = str + length - 1;    // p2指向字符串的最后一个字符
+
+    // 逐个对调第一个和最后一个字符、第二个和倒数第二字符
+    // 如此下去，直到达到字符串的中间
+    while (p1 < p2) {
+        // 对调字符
+        char c = *p1;
+        *p1 = *p2;
+        *p2 = c;
+        // 移动指针
+        ++p1;
+        --p2;
+    }
+
+    printf("string now is  %s\n", str);
+}
+
+void function_4_38() {
+    // 主函数测试findnumstring函数是否正常工作
+    char string[] = "abcd12345eee125ss123456789";
+    char *p = (char *) malloc(strlen(string) + 1);
+    int count = findnumstring(p, string);
+    printf("%s\nnumber string length = %d\n", p, count);
+}
+
+int findnumstring(char *outputstr, char *inputstr) {
+    /* temp用于指向正在搜索中的数字子串
+           final用于指向最终的最长的数字子串
+     */
+    char *in = inputstr, *out = outputstr, *temp, *final;
+    int count = 0, maxlen = 0;
+    int i;
+
+    while (*in != '\0')   //该循环用于找到最长的数字串，初始in指向输入串
+    {
+        // 如果in所指向的字符串的第一个字符为数字
+        // 就获取以此数字字符开始的数字串的长度
+        // 如果不是数字字符，in指向输入串的下一个字符
+        if (*in > 47 && *in < 58) {
+            // 注意in指针在变化
+            for (temp = in; *in > 47 && *in < 58; in++)
+                count++;
+        } else
+            in++;
+
+        // 如果temp所指向数字串的长度比上一次找到的长
+        // 则把当前找到的最长数字串长度和地址分别赋给maxlen和final
+        if (maxlen < count) {
+            maxlen = count;
+            final = temp;
+        }
+
+        // 把count清零
+        count = 0;
+    }
+
+    // while循环结束后，最长的数字串已经找到。
+    // 把找到的最长数字串存储到out所指的存储空间
+    for (i = 0; i < maxlen; i++) {
+        *out = *final;
+        out++;
+        final++;
+    }
+    *out = '\0';
+
+    return maxlen;
+}
+
 void function_4_37() {
     char *str = "NULL";
+
+    //指向函数内部变量的指针
     str = RetMemory();
     printf("%s", str);
 }
