a25654026e8f91e3d9d4b9f9d85b2fb004bd2374
lilizhao <lilizhao1@oneapm.com>
2019-08-23 09:41:46 +0800

    文件锁

--- chater_6/chapter_6.c
+++ chater_6/chapter_6.c
@@ -20,10 +20,88 @@ int my_read(int fd);
 
 void my_err(const char *err_string, int line);
 
+void fcntl_access();
+
+void fcntl_lock();
+
 int main(int argc, char **argv) {
 //    my_chomod(argc, argv);
 //    my_create();
-    my_rwl();
+//    my_rwl();
+//    fcntl_access();
+    fcntl_lock();
+}
+
+void fcntl_lock() {
+    int fd;
+    int ret;
+    struct flock lock;
+    char read_buf[32];
+
+    /*打开或创建新文件文件*/
+    if ((fd = open("/Users/oneapm/CLionProjects/hello_c/cmake-build-debug/lilizhao.c", O_CREAT | O_TRUNC | O_RDWR,
+                   S_IRWXU)) == -1) {
+        my_err("open", __LINE__);
+    }
+    if (write(fd, "test lock", 10) != 10) {
+        my_err("open", __LINE__);
+    }
+
+    /*初始化lock结构*/
+    memset(&lock, 0, sizeof(struct flock));
+    lock.l_start = SEEK_SET;
+    lock.l_whence = 0;
+    lock.l_len = 0;
+
+    //TODO 测试
+}
+
+/*自定义的错误处理函数*/
+void my_err(const char *err_string, int line) {
+    fprintf(stderr, "line:%d  ", line);
+    perror(err_string);
+    exit(1);
+}
+
+void fcntl_access() {
+    int ret;
+    int access_mode;
+    int fd;
+
+    if ((fd = open("/Users/oneapm/CLionProjects/hello_c/cmake-build-debug/lilizhao.c", O_CREAT | O_TRUNC | O_RDWR,
+                   S_IRWXU)) == -1) {
+        my_err("open", __LINE__);
+    }
+
+    /*设置文件打开方式*/
+    if ((ret = fcntl(fd, F_SETFL, O_APPEND)) < 0) {
+        my_err("fcntl", __LINE__);
+    }
+
+    /*获取文件打开方式*/
+    if ((ret = fcntl(fd, F_GETFL, 0)) < 0) {
+        my_err("fcntl", __LINE__);
+    }
+
+    access_mode = ret & O_ACCMODE;
+    if (access_mode == O_RDONLY) {
+        printf("example_64 access mode: read only");
+    } else if (access_mode == O_WRONLY) {
+        printf("example_64 access mode: write only");
+    } else if (access_mode == O_RDWR) {
+        printf("example_64 access mode: read + write");
+    }
+
+    if (ret & O_APPEND) {
+        printf(" ,append");
+    }
+    if (ret & O_NONBLOCK) {
+        printf(" ,nonblock");
+    }
+    if (ret & O_SYNC) {
+        printf(", sync");
+    }
+    printf("\n");
 }
 
 void my_rwl() {
@@ -56,13 +134,6 @@ void my_rwl() {
     my_read(fd);
 
     close(fd);
-}
-
-/*自定义的错误处理函数*/
-void my_err(const char *err_string, int line) {
-    fprintf(stderr, "line:%d  ", line);
-    perror(err_string);
-    exit(1);
 }
 
 /*自定义的读数据函数*/
