0025ec950d709198a60bfca3a8c7ceb87d9becd8
lilizhao <lilizhao1@oneapm.com>
2019-08-24 17:32:18 +0800

    读写锁测试

--- chater_6/chapter_6.c
+++ chater_6/chapter_6.c
@@ -24,6 +24,10 @@ void fcntl_access();
 
 void fcntl_lock();
 
+int lock_test(int fd, struct flock *lock);
+
+int lock_set(int fd, struct flock *lock);
+
 int main(int argc, char **argv) {
 //    my_chomod(argc, argv);
 //    my_create();
@@ -53,7 +57,73 @@ void fcntl_lock() {
     lock.l_whence = 0;
     lock.l_len = 0;
 
-    //TODO 测试
+    /*设置读锁*/
+    lock.l_type = F_RDLCK;
+    if (lock_test(fd, &lock) == 0) { // 测试可以设置锁
+        lock.l_type = F_RDLCK;
+        lock_set(fd, &lock);
+    }
+
+    /*读数据*/
+    lseek(fd, 0, SEEK_SET);
+    if ((ret = read(fd, read_buf, 10)) < 0) {
+        my_err("read", __LINE__);
+    }
+    read_buf[ret] = '\0';
+    printf("%s\n", read_buf);
+
+    /*等待任意按键*/
+    getchar();
+
+    /*设置写锁*/
+    lock.l_type = F_WRLCK;
+    if (lock_test(fd, &lock) == 0) { // 测试可以设置锁
+        lock.l_type = F_WRLCK;
+        lock_set(fd, &lock);
+    }
+
+    /*释放锁*/
+    lock.l_type = F_UNLCK;
+    lock_set(fd, &lock);
+    close(fd);
+}
+
+/*锁的设置或释放函数*/
+int lock_set(int fd, struct flock *lock) {
+    if (fcntl(fd, F_SETLK, lock) == 0) { // 执行成功
+        if (lock->l_type == F_RDLCK) {
+            printf("set read lock,pid:%d\n", getpid());
+        } else if (lock->l_type == F_WRLCK) {
+            printf("set write lock,pid:%d\n", getpid());
+        } else if (lock->l_type == F_UNLCK) {
+            printf("release lock,pid:%d\n", getpid());
+        }
+    } else {                          // 执行失败，返回-1
+        perror("lock operation fail\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+/*测试锁,只有当测试发现参数lock指定的锁能被设置时，返回0*/
+int lock_test(int fd, struct flock *lock) {
+    if (fcntl(fd, F_GETLK, lock) == 0) {    // 执行成功
+        if (lock->l_type == F_UNLCK) {    // 测试发现能按参数lock要求设置锁
+            printf("lock can be set in fd\n");
+            return 0;
+        } else {    // 有不兼容的锁存在，打印出设置该锁的进程ID
+            if (lock->l_type == F_RDLCK) {
+                printf("can't set lock, read lock has been set by:%d\n", lock->l_pid);
+            } else if (lock->l_type == F_WRLCK) {
+                printf("can't set lock, write lock has been set by:%d\n", lock->l_pid);
+            }
+            return -2;
+        }
+    } else {                                // 执行失败，返回-1
+        perror("get incompatible locks fail");
+        return -1;
+    }
 }
 
 /*自定义的错误处理函数*/
